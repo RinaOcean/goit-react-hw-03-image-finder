f1a198cfe883eb959117166c095b6308af018945
RinaOcean <antyfifa@gmail.com>
2021-04-27 21:36:09 +0300

    inintial start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './App.scss';
+
+class App extends Component {
+  // static propTypes = {
+
+  // };
+
+  // state = {
+
+  // };
+
+  render() {}
+}
+
+export default App;
